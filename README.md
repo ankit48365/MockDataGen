6404e840304cf3d484835bcc76896b66fde9d825
ankit K <ankit48365@gmail.com>
2024-05-11 07:18:16 -0400

    test1 working

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rdb_data_generation
 Code to generate Random Data following Key Integrity for a bunch of ER tied tables
+
+Local on Windows Git here --> E:\here\git
+Knowingly not creating venv for this project as - this wont be using many distinct python libraries and will be easy to trigger from airflow, when it will be ready
+
---
+++ code/test1.py
@@ -0,0 +1,41 @@
+import pyodbc
+import random
+from datetime import datetime, timedelta
+
+# Establish a connection to your database
+conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'
+                      'SERVER=localhost;'
+                      'DATABASE=POC;'
+                      'UID=poc_login;'
+                      'PWD=Dominos#2023')
+
+
+
+cursor = conn.cursor()
+
+# Get the maximum order_id
+cursor.execute("SELECT MAX([order_id]) AS MaxOrderID FROM [POC].[pizza].[orders]")
+max_id = cursor.fetchone()[0]
+
+# Get the current date
+current_date = datetime.now().strftime('%Y-%m-%d')
+
+# Generate 5 new records
+for i in range(1, 5):
+    # Generate a random time between 8:00:00 and 16:00:00
+    random_time = (datetime.now().replace(hour=8, minute=0, second=0) + timedelta(seconds=random.randint(0, 8*3600))).time()
+    
+    # Create the new order_id
+    new_order_id = max_id + i
+    
+    # Create the insert query
+    query = f"INSERT INTO [POC].[pizza].[orders] ([order_id],[date],[time]) VALUES('{new_order_id}','{current_date}', '{random_time}')"
+    
+    # Execute the query
+    cursor.execute(query)
+
+# Commit the changes
+conn.commit()
+
+# Close the connection
+conn.close()
