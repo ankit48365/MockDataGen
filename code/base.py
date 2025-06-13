e51fc8fef6967618a7f2b45136df676155847558
ankit K <ankit48365@gmail.com>
2025-06-12 21:13:32 -0400

    A

--- code/base.py
+++ code/base.py
@@ -1,29 +1,58 @@
 from faker import Faker
+from tabulate import tabulate
 import pandas as pd
 
 fake = Faker()
 
-for _ in range(10):
+def main():
+    pass
+
+
+def generate_user_data(num_of_records: int) -> pd.DataFrame:
     
-    user_data = {
+    user_data = [{
         "first_name": fake.first_name(),
         "middle_name": fake.first_name(),
         "last_name": fake.last_name(),
-        "address_line1": fake.street_address(),
+        "address": fake.street_address(),
         "city": fake.city(),
         "state": fake.state(),
         "zip_code": fake.zipcode(),
         "phone": fake.phone_number(),
         "email": fake.email()
-    }
+    } for _ in range(num_of_records)
+    ]
 
-    # print(user_data)
+    user_df = pd.DataFrame(user_data)
 
-    # append each row to a DataFrame
-    user_df = pd.DataFrame([user_data])
-    if 'user_df' in locals():
-        user_df = pd.concat([user_df, user_df], ignore_index=True)
-    else:
-        user_df = user_df
+    return user_df
 
-print(user_df)
+def print_user_data(records_to_print: int) -> None:
+    user_df = generate_user_data(records_to_print)
+    print(tabulate(user_df, headers='keys', tablefmt='psql', showindex=False)) # grid
+
+
+if __name__ == "__main__":
+
+    num_of_records = int(input("Enter the number of records to generate: "))
+    print_user_data(num_of_records)
+
+
+
+
+
+
+
+
+
+
+# def generate_address_variations(base_address):
+#     variations = [
+#         base_address,
+#         base_address.replace("Street", "St."),
+#         base_address.replace("Avenue", "Ave."),
+#         base_address.replace("Road", "Rd."),
+#         base_address.replace("Boulevard", "Blvd."),
+#         base_address.lower().title(),  # Capitalize properly
+
+# does fake has street address2?
