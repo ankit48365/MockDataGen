ceed360554b8b15c8e54e5e6ca7b739324e27a3e
ankit K <ankit48365@gmail.com>
2025-06-13 11:07:02 -0400

    X

--- myapp/base.py
+++ myapp/base.py
@@ -24,15 +24,29 @@ def generate_user_data(num_of_records: int) -> pd.DataFrame:
 
     return user_df
 
-def print_user_data(records_to_print: int) -> None:
-    user_df = generate_user_data(records_to_print)
+def print_user_data(records_to_process: int) -> None:
+    user_df = generate_user_data(records_to_process)
     print(tabulate(user_df, headers='keys', tablefmt='psql', showindex=False)) # grid
+
+def csv_user_data(records_to_process: int, filename: str = "user_data.csv") -> pd.DataFrame:
+    user_df = generate_user_data(records_to_process)
+    user_df.to_csv(filename, index=False)
+    print(f"User data saved to {filename}")
+    return user_df
+
+def mdm_split_data_set(records_to_process: int) -> None:
+    user_df = csv_user_data(records_to_process)
+    df_20 = user_df.sample(frac=0.2, random_state=42)  # Pick 20% randomly
+    df_80 = user_df.drop(df_20.index)  # Remaining 80%  , droping the one used in df_20
+    df_20.to_csv("user_data_20.csv", index=False)
+    df_80.to_csv("user_data_80.csv", index=False)
 
 
 if __name__ == "__main__":
 
     num_of_records = int(input("Enter the number of records to generate: "))
     print_user_data(num_of_records)
+    mdm_split_data_set(num_of_records)
 
 
 
