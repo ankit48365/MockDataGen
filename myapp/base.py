739f23743258b7d79f9651544af305603352e7ef
ankit K <ankit48365@gmail.com>
2025-06-14 19:20:28 -0400

    C

--- myapp/base.py
+++ myapp/base.py
@@ -22,53 +22,28 @@ def generate_user_data(num_of_records: int) -> pd.DataFrame:
     ]
 
     user_df = pd.DataFrame(user_data)
-    print(f"Number of records in main set: {user_df.shape[0]}")
-    print(f"Number of Unique Records in main set: {user_df.drop_duplicates().shape[0]}")
+    # print(f"Number of records in main set: {user_df.shape[0]}")
+    # print(f"Number of Unique Records in main set: {user_df.drop_duplicates().shape[0]}")
     user_df["Original"] = "Y" # Mark original records, this helps us later to identify the original records
-    user_df.to_csv("Data_Set_1.csv", index=False)
-    print(f"User data saved to {"Data_Set_1.csv"}")
-    if show_output == "Y":
-        print(tabulate(user_df, headers='keys', tablefmt='psql', showindex=False)) # grid
-    else:
-        print("Data not printed, only saved to CSV.")
+
     return user_df
 
-# def print_user_data(records_to_process: int) -> None:
-#     user_df = generate_user_data(records_to_process)
-#     print(tabulate(user_df, headers='keys', tablefmt='psql', showindex=False)) # grid
 
-# def csv_user_data(records_to_process: int, show_output: str, filename: str = "Data_Set_1.csv") -> pd.DataFrame:
-#     user_df["Original"] = "Y" # Mark original records, this helps us later to identify the original records
-#     user_df.to_csv(filename, index=False)
-#     print(f"User data saved to {filename}")
-#     # if show_output == "Y":
-#     #     print(tabulate(user_df, headers='keys', tablefmt='psql', showindex=False)) # grid
-#     # else:
-#     #     print("Data not printed, only saved to CSV.")
-#     return user_df
+# def mdm_split_data_set(records_to_process: int, show_output: str, user_df ) -> tuple[pd.DataFrame, pd.DataFrame]:
+def mdm_split_data_set(user_df) -> tuple[pd.DataFrame, pd.DataFrame]:
 
-def mdm_split_data_set(records_to_process: int, show_output: str) -> tuple[pd.DataFrame, int]:
-    user_df = generate_user_data(records_to_process)
-
-    # user_df = csv_user_data(records_to_process, show_output)
-
+    # user_df = generate_user_data(records_to_process)
     df_20 = user_df.sample(frac=0.2, random_state=42)  # Pick 20% randomly
     df_80 = user_df.drop(df_20.index)  # Remaining 80%  , droping the one used in df_20
-    df_20.to_csv("Data_Set_2_20_Unique.csv", index=False)
-    df20_num_records = df_20.shape[0]
-    # print(f"Number of records in 20 set: {df20_num_records}")
-    df_80.to_csv("Data_Set_2_80_Unique.csv", index=False)
-    df80_num_records = df_80.shape[0]
-    print(f"Number of records in 80 set: {df80_num_records}")
-    return df_20, df20_num_records
+    return df_20, df_80
 
-# def change_df_20(user_df: pd.DataFrame) -> pd.DataFrame:
-def change_df_20(records_to_process: int, show_output: str) -> pd.DataFrame:
-    df_20,df20_num_records = mdm_split_data_set(records_to_process, show_output)
-    print(f"Number of records in 20 set: {df20_num_records}")
+def change_df_20(df_20p: pd.DataFrame) -> pd.DataFrame:
+# def change_df_20(records_to_process: int, show_output: str) -> pd.DataFrame:
+    # df_20,df20_num_records = mdm_split_data_set(records_to_process, show_output)
+    # print(f"Number of records in 20 set: {df20_num_records}")
 
     # Store original rows to avoid index issues when DataFrame grows
-    original_rows = df_20.copy()
+    original_rows = df_20p.copy()
 
     name_map = {
     "Alexander": "Alex",
@@ -136,15 +111,6 @@ def change_df_20(records_to_process: int, show_output: str) -> pd.DataFrame:
             new_row = row.copy()  # Copy the current row directly
             # want to update the middle_name field and original field to value blank
             new_row["Original"] = "N"  # Mark as modified
-            # new_row["middle_name"] = ""   # Modify the name
-            # new_row["middle_name"] = random.choice(["", new_row["middle_name"][0]])
-
-            # Modify first name field based on dictionary
-            # if new_row["first_name"] in name_map:
-            #     new_row["first_name"] = random.choice([new_row["first_name"], name_map[new_row["first_name"]]])
-
-            # if new_row["first_name"] in name_map:
-            #     new_row["first_name"] = name_map[new_row["first_name"]]
 
             if new_row["first_name"] in name_map:
                 new_row["first_name"] = random.choice([name_map[new_row["first_name"]], new_row["first_name"]])
@@ -152,11 +118,6 @@ def change_df_20(records_to_process: int, show_output: str) -> pd.DataFrame:
             if new_row["middle_name"] in name_map:
                 new_row["middle_name"] = random.choice([name_map[new_row["middle_name"]], new_row["middle_name"], new_row["middle_name"][0]])
 
-            # new_row["first_name"] = random.choice([
-            #     name_map.get(new_row["first_name"], new_row["first_name"]),  # Use original if not found
-            #     new_row["middle_name"],
-            #     new_row["middle_name"][0] if new_row["middle_name"] else ""
-            # ])
 
             new_row["phone"] = random.choice([new_row["phone"], "", fake.phone_number()])
             new_row["email"] = random.choice([new_row["email"], "", fake.email()])
@@ -172,53 +133,57 @@ def change_df_20(records_to_process: int, show_output: str) -> pd.DataFrame:
             if "Drives" in new_row["address"]:
                 new_row["address"] = random.choice([new_row["address"].replace("Drives", "Dr."), new_row["address"].replace("Drives", "Drive")])    
 
-            df_20 = pd.concat([df_20, pd.DataFrame([new_row])], ignore_index=True)
+            df_20p = pd.concat([df_20p, pd.DataFrame([new_row])], ignore_index=True)
 
-    # Save the modified DataFrame
-    df_20.to_csv("Data_Set_2_20_modified.csv", index=False)
-    print(f"Modified 20% dataset saved with {df_20.shape[0]} total records")
-    
+            return df_20p
+
+
+def main():
+    num_of_records = int(input("Enter the number of records to generate: "))
+    show_output = input("Do you want to print the output? (Y/N): ").strip().upper()
+
+    ##############################################################################################
+    # Step 1 - Generate user data
+    print("\n####################################### STEP 1 ######################################\n")
+    user_df = generate_user_data(num_of_records)
+    user_df.to_csv("Data_Set_1_100.csv", index=False)
+    user_df_num_records = user_df.shape[0]
+    print(f"Number of records Full Data Set: {user_df_num_records} & Unique records as {user_df.drop_duplicates().shape[0]}, saved here {"Data_Set_1_100.csv"}")
+
     if show_output == "Y":
-        print(tabulate(df_20, headers='keys', tablefmt='psql', showindex=False)) # grid
+        print(tabulate(user_df, headers='keys', tablefmt='psql', showindex=False)) # grid
     else:
         print("Data not printed, only saved to CSV.")
     
-    return df_20
+    ###############################################################################################
+    # Step 2 - Split dataset in two data set 20 & 80
+    print("\n####################################### STEP 2 #######################################\n")
+    df_20, df_80 = mdm_split_data_set(user_df)
+    df_20.to_csv("Data_Set_2_20_Unique.csv", index=False)
+    df_80.to_csv("Data_Set_2_80_Unique.csv", index=False)
+    df20_num_records = df_20.shape[0]
+    df80_num_records = df_80.shape[0]
+    print(f"Number of records in 80 set: {df80_num_records}, saved here {"Data_Set_2_80_Unique.csv"}")
+    print(f"Number of records in 20 set: {df20_num_records}, saved here {"Data_Set_2_20_Unique.csv"}")
 
+    ###############################################################################################
+    # Step 3 - Create Data Set 20 with bad data added (it will have more then 20% of data)
+    print("\n####################################### STEP 3 #######################################\n")
+    df_20p = change_df_20(df_20)
+    df_20p.to_csv("Data_Set_2_20_modified.csv", index=False)
+    df_20p_num_records = df_20p.shape[0]
+    print(f"Number of records in Modified 20 set: {df_20p_num_records}, saved here {"Data_Set_2_20_modified.csv"}")
 
-
-# def u_middle_name(a, b):
-#     operation = random.choice(["+", "-", "*"])
-#     if operation == "+":
-#         return a + b
-#     elif operation == "-":
-#         return a - b
-#     else:  # Multiplication
-#         return a * b
-
-# def u_middlename(user_df, middle_name_value, original_value):
-
-#     user_df["middle_name"] = middle_name_value
-#     user_df["Original"] = original_value
-#     return user_df
-
-
+    if show_output == "Y":
+        print(tabulate(df_20p, headers='keys', tablefmt='psql', showindex=False)) # grid
+    else:
+        print("Data not printed, only saved to CSV.")
 
 if __name__ == "__main__":
+    main()
 
-    num_of_records = int(input("Enter the number of records to generate: "))
-    show_output = "Y"
-    # print_user_data(num_of_records)
-    change_df_20(num_of_records, show_output)
+    # num_of_records = int(input("Enter the number of records to generate: "))
+    # show_output = "N"
+    # # print_user_data(num_of_records)
+    # change_df_20(num_of_records, show_output)
 
-
-# def generate_address_variations(base_address):
-#     variations = [
-#         base_address,
-#         base_address.replace("Street", "St."),
-#         base_address.replace("Avenue", "Ave."),
-#         base_address.replace("Road", "Rd."),
-#         base_address.replace("Boulevard", "Blvd."),
-#         base_address.lower().title(),  # Capitalize properly
-
-# does fake has street address2?
